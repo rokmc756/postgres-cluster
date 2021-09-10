ac092c33ebf776149be722b933d27b57712ea6f5
Jack Moon <rokmc756@gmail.com>
2021-09-10 22:49:53 +0900

    Test

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # What is PostgreSQL Cluster
 postgres-cluster provide ansible playbook to deploy three kind of cluster such as gpfailover, patroni, mutlti master ( bdr ) with haproxy and etcd.
+
+
+# How to install PostgreSQL BDR in postgres-cluster
+~~
+- hosts: all
+  roles:
+    - pgsql94-bdr
+    - haproxy
+
+- hosts: postgres-haproxy01
+  roles:
+     - { role: keepalived, keepalived_shared_ip: "192.168.0.79", keepalived_role: "master" }
+
+- hosts: postgres-haproxy02
+  roles:
+     - { role: keepalived, keepalived_shared_ip: "192.168.0.80", keepalived_role: "slave" }
+~~
+
--- remove_force_pgautofailover.sh
+++ remove_force_pgautofailover.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-for i in `echo "80 81 82 83"`
+for i in `echo "79 80 81 82 83"`
 do
-    ssh root@192.168.0.$i "systemctl stop patroni; killall etcd; rm -rf /var/lib/pgsql/* ; rm -rf /var/lib/pgsql/.* ; rpm -Uvh /home/jomoon/vmware-postgres-11.12-0.el7.x86_64.rpm --force ; ls -al /var/lib/pgsql"
-    # ssh root@192.168.0.$i "systemctl stop pgautofailover; rm -rf /var/lib/pgsql/* ; rm -rf /var/lib/pgsql/.* ; rpm -Uvh /home/jomoon/vmware-postgres-11.12-0.el7.x86_64.rpm --force ; ls -al /var/lib/pgsql"
+    #ssh root@192.168.0.$i "systemctl stop patroni"
+    #ssh root@192.168.0.$i "killall etcd"
+    #ssh root@192.168.0.$i "rm -rf /var/lib/pgsql/*"
+    #ssh root@192.168.0.$i "rm -rf /var/lib/pgsql/.*"
+    #ssh root@192.168.0.$i "yum remove -y vmware-postgres"
+    #ssh root@192.168.0.$i "ls -al /var/lib/pgsql"
+    ssh root@192.168.0.$i "systemctl stop pgautofailover; killall postgres; yum remove -y vmware-postgres ; rm -rf /var/lib/pgsql/* ; rm -rf /var/lib/pgsql/.* ; ls -al /var/lib/pgsql"
+    # ssh root@192.168.0.$i "systemctl stop pgautofailover; killall etcd; killall postgres; yum remove -y vmware-postgres ; rm -rf /var/lib/pgsql/* ; rm -rf /var/lib/pgsql/.* ; ls -al /var/lib/pgsql"
 done
